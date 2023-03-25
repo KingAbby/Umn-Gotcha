76a5b18e330ece16fb3ae7c23abb818dcabf615f
KingAbby <110515353+KingAbby@users.noreply.github.com>
2023-03-25 23:07:37 +0700

    Mentahan umngotcha
    kalo mau ubah jgn lupa di update

--- script.js
+++ script.js
@@ -1,53 +1,202 @@
-// Variables to keep track of pet's status
-let petName = "Pou";
-let hunger = 50;
-let energy = 50;
-let cleanliness = 50;
+/* Tampilan Umngotcha */
 
-// Variables for game elements
-const petContainer = document.getElementById("pet-container");
-const pet = document.getElementById("pet");
-const petNameSpan = document.getElementById("pet-name");
-const hungerSpan = document.getElementById("hunger");
-const energySpan = document.getElementById("energy");
-const cleanlinessSpan = document.getElementById("cleanliness");
-const feedButton = document.getElementById("feed-button");
-const playButton = document.getElementById("play-button");
-const bathButton = document.getElementById("bath-button");
+/* Inisialisasi variabel */
+let petName = "";
+let petImage = "";
+let petHealth = 100;
+let petHunger = 100;
+let petSleepiness = 100;
+let petHappiness = 100;
+let petLevel = 1;
+let petExp = 0;
+let time = 0;
+let hour = 0;
+let activityTime = 15000; // 15 detik
 
-// Function to update pet's status and display it on screen
-function updatePetStatus() {
-  petNameSpan.textContent = petName;
-  hungerSpan.textContent = hunger;
-  energySpan.textContent = energy;
-  cleanlinessSpan.textContent = cleanliness;
+/* Pilihan gambar avatar hewan */
+let avatarImages = [
+  "https://via.placeholder.com/200x200",
+  "https://via.placeholder.com/200x200",
+  "https://via.placeholder.com/200x200",
+  "https://via.placeholder.com/200x200"
+];
+// Ubah variabel waktu supaya menjadi 1 detik irl = 1 menit in-game
+
+/* Inisialisasi waktu */
+function initTime() {
+  setInterval(updateTime, 1000);
 }
 
-// Function to feed the pet
-function feedPet() {
-  hunger += 10;
-  energy -= 5;
-  updatePetStatus();
+/* Update waktu */
+function updateTime() {
+  time += 1;
+  hour = time % 24;
+
+  /* Update jam pada tampilan */
+  let timeElement = document.getElementById("time");
+  let greetingElement = document.getElementById("greeting");
+
+  if (hour >= 5 && hour < 12) {
+    timeElement.textContent = "Good Morning!";
+    greetingElement.textContent = "Good Morning!";
+    setBackground("bg-morning");
+  } else if (hour >= 12 && hour < 18) {
+    timeElement.textContent = "Good Afternoon!";
+    greetingElement.textContent = "Good Afternoon!";
+    setBackground("bg-afternoon");
+  } else {
+    timeElement.textContent = "Good Night!";
+    greetingElement.textContent = "Good Night!";
+    setBackground("bg-night");
+  }
 }
 
-// Function to play with the pet
-function playWithPet() {
-  energy += 10;
-  cleanliness -= 5;
-  updatePetStatus();
+/* Update tampilan */
+function updateDisplay() {
+  /* Update avatar */
+  let petImageElement = document.getElementById("pet-image");
+  petImageElement.style.backgroundImage = `url(${petImage})`;
+
+  /* Update status bar */
+  let healthBar = document.getElementById("health-bar");
+  healthBar.style.width = `${petHealth}%`;
+
+  let hungerBar = document.getElementById("hunger-bar");
+  hungerBar.style.width = `${petHunger}%`;
+
+  let sleepinessBar = document.getElementById("sleepiness-bar");
+  sleepinessBar.style.width = `${petSleepiness}%`;
+
+  let happinessBar = document.getElementById("happiness-bar");
+  happinessBar.style.width = `${petHappiness}%`;
+
+  let levelElement = document.getElementById("level");
+  levelElement.textContent = `Level ${petLevel} (${petExp} XP)`;
 }
 
-// Function to bath the pet
-function bathPet() {
-  cleanliness += 10;
-  energy -= 5;
-  updatePetStatus();
+/* Inisialisasi tampilan */
+function initDisplay() {
+  /* Set nama hewan piaraan */
+  let petNameElement = document.getElementById("pet-name");
+  petNameElement.textContent = petName;
+
+  /* Set gambar avatar */
+  petImage = avatarImages[Math.floor(Math.random() * avatarImages.length)];
+  let petImageElement = document.getElementById("pet-image");
+  petImageElement.style.backgroundImage = `url(${petImage})`;
+
+  /* Update tampilan */
+  updateDisplay();
+
+  /* Inisialisasi waktu */
+  initTime();
 }
 
-// Event listeners for game buttons
-feedButton.addEventListener("click", feedPet);
-playButton.addEventListener("click", playWithPet);
-bathButton.addEventListener("click", bathPet);
+/* Update kondisi hewan piaraan */
+function updatePetCondition() {
+  /* Kurangi kondisi setiap kali update */
+  petHunger -= 10;
+  petSleepiness -= 5;
+  petHappiness -= 10;
 
-// Initialize the pet status
-updatePetStatus();
+  /* Update kondisi kesehatan */
+  let health = (petHunger + petSleepiness + petHappiness) / 3;
+  petHealth = health;
+}
+
+/* cek kondisi kesehatan */
+function cekKesehatan() {
+    if (kesehatan <= 0) {
+    clearInterval(interval);
+    clearInterval(waktu);
+    alert("Game over, " + namaHewan + "meninggal karena sakit");
+    btnAktivitas.forEach((btn) => (btn.disabled = true));
+    } else if (kesehatan < 30) {
+    kondisiHewan.innerHTML = "Kesehatan: ";
+    progressKesehatan.classList.add("bg-danger");
+    } else if (kesehatan < 60) {
+    kondisiHewan.innerHTML = "Kesehatan: ";
+    progressKesehatan.classList.add("bg-warning");
+    } else {
+    kondisiHewan.innerHTML = "Kesehatan: ";
+    progressKesehatan.classList.add("bg-success");
+    }
+    }
+    
+    /* update kondisi pemain */
+    function updateKondisi() {
+    makananBar.style.width = makanan + "%";
+    tidurBar.style.width = tidur + "%";
+    mainBar.style.width = main + "%";
+    kesehatanBar.style.width = kesehatan + "%";
+    
+    cekKondisiLevel();
+    cekKesehatan();
+    }
+    
+    /* mengurangi kondisi pemain secara berkala */
+    function kurangKondisi() {
+    makanan -= 5;
+    tidur -= 10;
+    main -= 7;
+    kesehatan -= 2;
+    
+    updateKondisi();
+    }
+    
+    /* aktivitas makan */
+    btnMakan.addEventListener("click", () => {
+    makan += 30;
+    kesehatan += 10;
+    kondisiHewan.innerHTML = "Makan: ";
+    avatarHewan.src = "img/hewan-makan.gif";
+    setTimeout(() => {
+    avatarHewan.src = avatarMood();
+    }, 15000);
+    });
+    
+    /* aktivitas tidur */
+    btnTidur.addEventListener("click", () => {
+    tidur += 30;
+    makan -= 10;
+    kondisiHewan.innerHTML = "Tidur: ";
+    avatarHewan.src = "img/hewan-tidur.gif";
+    setTimeout(() => {
+    avatarHewan.src = avatarMood();
+    }, 30000);
+    });
+    
+    /* aktivitas main */
+    btnMain.addEventListener("click", () => {
+    main += 20;
+    makan -= 5;
+    kondisiHewan.innerHTML = "Main: ";
+    avatarHewan.src = "img/hewan-main.gif";
+    setTimeout(() => {
+    avatarHewan.src = avatarMood();
+    }, 20000);
+    });
+    
+    /* aktivitas minum obat */
+    btnObat.addEventListener("click", () => {
+    kesehatan += 20;
+    kondisiHewan.innerHTML = "Obat: ";
+    avatarHewan.src = "img/hewan-sehat.gif";
+    setTimeout(() => {
+    avatarHewan.src = avatarMood();
+    }, 5000);
+    });
+    
+    /* set interval mengurangi kondisi pemain secara berkala */
+    const interval = setInterval(() => {
+    kurangKondisi();
+    }, 10000);
+    
+    /* set interval update waktu */
+    const waktu = setInterval(() => {
+    updateWaktu();
+    }, 1000);
+    
+    
+    
+    
